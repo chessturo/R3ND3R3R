68c743aa1426c95fd8812d635ec4c401a49c22cf
levymitchell0 <levymitchell0@gmail.com>
2024-03-12 12:11:43 -0700

    Incremental progress on image approximation

--- ascii_render/__main__.py
+++ ascii_render/__main__.py
@@ -1,9 +1,23 @@
 import sys
+
+import monobit
+from monobit.streams import Stream
+from monobit.formats.text.yaff import load_yaff
+
 from .exr import openImage, writeImage
-from .fontToMap import create_arrays
-from .imageProcessing import applyApproximation
+from .fontToMap import FontMap
+from .imageProcessing import apply_approximation
+
+debug = True
 
 image = openImage(sys.argv[1])
-font = create_arrays(sys.argv[2])
-applyApproximation(image, font)
+# font = FontMap(sys.argv[2])
+# font = load_yaff(Stream(open(sys.argv[2], 'r'), 'r'))
+font = monobit.load(sys.argv[2]).get()
+lines = apply_approximation(image, font)
+if debug:
+    import functools
+    text = functools.reduce(lambda x, y: x + y, lines)
+    f = open('test/output.txt', 'wb')
+    f.write(text.encode())
 writeImage(sys.argv[3], image)
--- ascii_render/imageProcessing.py
+++ ascii_render/imageProcessing.py
@@ -1,29 +1,43 @@
-from typing import List
+import typing
 
+from monobit import Font, Glyph
 from numpy import ndarray, linalg as la
 import numpy as np
 
-def computeDistance(m1: ndarray, m2: ndarray):
+
+def compute_distance(m1: ndarray, m2: ndarray):
     diff = m1 - m2
     return la.norm(diff)
 
-def bestApproximation(m1: ndarray, m2: List[List[int]]):
+
+def best_approximation(m1: ndarray, m2: Font):
     least_dist = float('inf')
     best_approx = None
-    for mat in m2:
-        glyph = np.reshape(mat, (16, 8))
-        dist = computeDistance(m1, glyph)
+    best_approx_char = None
+    for glyph in m2.glyphs:
+        if glyph.height != 16 or glyph.width != 8:
+            continue
+        bits = glyph.as_matrix()
+        mat = np.reshape(bits, (16, 8))
+        dist = compute_distance(m1, mat)
         if dist < least_dist:
             least_dist = dist
             best_approx = mat
-    return np.reshape(best_approx, (16,8))
+            best_approx_char = glyph.char
+    return np.reshape(best_approx, (16, 8)), best_approx_char
 
 
-def applyApproximation(img: ndarray, glyphs: List[List[int]]):
+def apply_approximation(img: ndarray, glyphs: Font):
     blocks_wide = img.shape[1] // 8
     blocks_high = img.shape[0] // 16
-
-    for i in range(blocks_wide):
-        for j in range(blocks_high):
+    lines = []
+    for j in range(blocks_high):
+        lines.append('')
+        for i in range(blocks_wide):
             block = img[j * 16:(j + 1) * 16, i * 8:(i + 1) * 8]
-            img[j * 16:(j + 1) * 16, i * 8:(i + 1) * 8] = bestApproximation(block, glyphs)
+            (newblock, char) = best_approximation(block, glyphs)
+            img[j * 16:(j + 1) * 16, i * 8:(i + 1) * 8] = newblock
+            lines[j] += char
+        lines[-1] += '\n'
+
+    return lines
